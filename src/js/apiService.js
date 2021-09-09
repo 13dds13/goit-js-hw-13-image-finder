fbb33e23fec51acbd058c28d0c656efdcf6daebe
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-09-09 13:50:05 +0300

    add show more btn

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,14 +1,14 @@
 // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
 // 23300016-a284ec07e988de3edbd291fb8 api-ket
 
-export default async function (query) {
+export default async function (query, page = 1) {
     const BASE_URL = 'https://pixabay.com/api';
     
     const queryParams = new URLSearchParams({
         image_type: 'photo',
         orientation: 'horizontal',
         q: [query],
-        page: 1,
+        page: [page],
         per_page: 12,
         key: '23300016-a284ec07e988de3edbd291fb8',
     });
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,38 @@
-import testFetch from './apiService';
+import newFetch from './apiService';
 import imgsMarkup from '../templates/photo-card.hbs'
+const debounce = require('lodash.debounce');
 
+let currentPage = 1;
+let currentQuery = '';
 
-const input = document.querySelector('#search-form');
+const searchDiv = document.querySelector('#search-container');
+const form = document.querySelector('#search-form');
+const galleryList = document.querySelector('.gallery');
+const showMoreBtn = document.querySelector('#load-btn');
 
-const onInput = (e) => {
+const onInput = e => {
     const query = e.target.value;
+    
+    if (!query) {
+        return;
+    }
 
-    testFetch(query)
-    .then(res => document.querySelector('.gallery').insertAdjacentHTML('beforeend', imgsMarkup(res)));
+    currentQuery = query;
+    newFetch(query)
+        .then(res => {
+            galleryList.innerHTML = imgsMarkup(res);
+            showMoreBtn.classList.remove('is-hidden');
+        });
+};
 
-}
 
-// input.addEventListener('input', onInput);  debounce!!!!!!!!!
+const onShowMoreBtnClick = () => {
+    currentPage += 1;
+    newFetch(currentQuery, currentPage)
+        .then(res => galleryList.insertAdjacentHTML('beforeend', imgsMarkup(res)));
+
+};
+
+form.addEventListener('input', debounce(onInput, 750));
+showMoreBtn.addEventListener('click', onShowMoreBtnClick);
 
